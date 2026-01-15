72d8b78a48a29e22bd481a2460561c13b1a89999
ajuvi <102821845+ajuvi@users.noreply.github.com>
2026-01-15 19:40:26 +0100

    init

---
+++ Mark/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace Mark
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ENTRA UNA NOTA: ");
+            int nota = Convert.ToInt32(Console.ReadLine());
+            string notaText = CalcularNotaText(nota);
+            Console.WriteLine(notaText);
+        }
+
+        /// <summary>
+        /// Transforma una nota numèrica en una nota en format text.
+        /// </summary>
+        /// <param name="nota">Nota numèrica entera.</param>
+        /// <returns>Nota en format text</returns>
+        /// <exception cref="Exception">La nota numèrica ha de ser 
+        /// un valor de l'interval [0,10]</exception>
+        public static string CalcularNotaText(int nota)
+        {
+            if (nota < 0 || nota > 10)
+                throw new Exception("Nota no vàlida");
+
+            string notaStr = "";
+            if (nota > 8) notaStr = "Excel·lent";
+            else if (nota > 6) notaStr = "Notable";
+            else if (nota > 4) notaStr = "Satisfactori";
+            else notaStr = "No assolit";
+
+            return notaStr;                
+        }
+    }
+}
---
+++ MarkTests/Test1.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mark;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mark.Tests
+{
+    [TestClass()]
+    public class Test1
+    {
+        [TestMethod()]
+        [DataRow(0, "No assolit")]
+        [DataRow(4, "No assolit")]
+        [DataRow(6, "Satisfactori")]
+        [DataRow(8, "Notable")]
+        [DataRow(9, "Excel·lent")]
+        public void CalcularNotaTextTest(int num, string expected)
+        {
+
+            //Act
+            string actual = Program.CalcularNotaText(num);
+            
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow(-1)]
+        [DataRow(-6)]
+        [DataRow(11)]
+        [ExpectedException(typeof(Exception))]
+        public void CalcularNotaText_Excepcions(int nota)
+        {
+            Program.CalcularNotaText(nota);
+        }
+        //private static bool CheckException(Func<object?> action)
+        //{
+        //    try { action.Invoke(); } catch (Exception ex) { return true; }
+        //    return false;
+        //}
+
+        //[TestMethod]
+        //public void CalcularNotaText_Excepcions()
+        //{
+        //    Assert.IsTrue(CheckException(() => Program.CalcularNotaText(-1)));
+        //    Assert.IsTrue(CheckException(() => Program.CalcularNotaText(11)));
+        //}
+
+        //private static bool CheckException(Func<object?> action)
+        //{
+        //    try { action.Invoke(); } catch (Exception ex) { return true; }
+        //    return false;
+        //}
+
+        //[TestMethod]
+        //public void CalcularNotaText_Excepcions()
+        //{
+        //    Assert.IsTrue(CheckException(()=>Program.CalcularNotaText(-1)));
+        //    Assert.IsTrue(CheckException(() => Program.CalcularNotaText(11)));
+        //}
+
+        //private static bool CheckException(Func<object?> action)
+        //{
+        //    try { action.Invoke(); } catch (Exception ex) { return true; }
+        //    return false;
+        //}
+
+    }
+}
